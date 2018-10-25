c309b64627e921acdf48b7e45fcf9782b1b29b7f
victorcerutti <victor.cerutti@gmail.com>
2018-10-25 12:52:55 +0200

    More tests

--- test.py
+++ test.py
@@ -1,30 +1,55 @@
 from meteofrance.client import meteofranceClient, meteofranceError
 import time
 
+#marseille : no rain forecast
 try:
     client = meteofranceClient(13000)
     print(client.get_data())
 except meteofranceError as exp:
     print(exp)
 
+#Rouen : rain forecast available
 try:
-    client = meteofranceClient(95750)
+    client = meteofranceClient(76000)
+    print(client.get_data())
+except meteofranceError as exp:
+    print(exp)
+
+#postal code is not correct : should return the first result which is "Ableiges"
+try:
+    client = meteofranceClient(95)
+    print(client.get_data())
+except meteofranceError as exp:
+    print(exp)
+
+#postal code is a city name : should work anyway with Brest forecast
+try:
+    client = meteofranceClient("Brest")
+    print(client.get_data())
+except meteofranceError as exp:
+    print(exp)
+
+#postal code is not correct : should return an error
+try:
+    client = meteofranceClient("foo")
+    print(client.get_data())
+except meteofranceError as exp:
+    print(exp)
+
+#pointe-à-pitre : result from météo-france is different and it returns less data
+try:
+    client = meteofranceClient(97110)
     print(client.get_data())
 except meteofranceError as exp:
     print(exp)
 
 
 """
-mf=meteofrance(13000)
-print mf.get_data()
-
-mf=meteofrance(76000)
-print mf.get_data()
-
-print "waiting 5 minutes to show update"
+#if needed, test update
+print("waiting 5 minutes to show update")
 time.sleep(300)
-mf.update()
-print mf.get_data()
+client.update()
+print(client.get_data())
 """
 
 
