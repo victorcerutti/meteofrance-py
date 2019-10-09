4b0212df384f69fa45e743a213debd2e1a7b7ff0
Oncleben31 <oncleben31@users.noreply.github.com>
2019-10-09 21:57:22 +0200

    Prepare next package version 0.3.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='meteofrance',
-    version='0.3.7',
+    version='0.3.8',
     description = 'Meteo-France weather forecast',
     author = 'victorcerutti',
     author_email = 'victorcerutti+meteofrance@gmail.com',
